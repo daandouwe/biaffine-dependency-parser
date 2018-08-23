b79813bce34628a97edc2c145b5017d7261cfcb2
daandouwe <daan_douwe@outlook.com>
2018-08-23 12:38:38 +0200

    Now can use ~ for home directory.

--- data.py
+++ data.py
@@ -209,11 +209,11 @@ class Data:
 class Corpus:
     """A corpus of a dictionary and three datasets (train, development, and test)."""
     def __init__(self, vocab_path="vocab/train", data_path="~/data/stanford-ptb/", char=False):
-        data_path = os.path.expanduser(data_path) if data_path.startswith('~') else data_path
+        data_path = os.path.expanduser(data_path)
         self.dictionary = Dictionary(vocab_path, char=char)
-        self.train  = Data(os.path.join(data_path, "train-stanford-raw.conll"), self.dictionary, char=char)
-        self.dev    = Data(os.path.join(data_path, "dev-stanford-raw.conll"), self.dictionary, char=char)
-        self.test   = Data(os.path.join(data_path, "test-stanford-raw.conll"), self.dictionary, char=char)
+        self.train = Data(os.path.join(data_path, "train-stanford-raw.conll"), self.dictionary, char=char)
+        self.dev = Data(os.path.join(data_path, "dev-stanford-raw.conll"), self.dictionary, char=char)
+        self.test = Data(os.path.join(data_path, "test-stanford-raw.conll"), self.dictionary, char=char)
 
 if __name__ == "__main__":
     # Example usage:
@@ -222,8 +222,7 @@ if __name__ == "__main__":
     for _ in range(10):
         words, tags, heads, labels = next(batches)
 
+    # Test character model on data.
     from nn import RecurrentCharEmbedding
-
     model = RecurrentCharEmbedding(len(corpus.dictionary.w2i), 100, 100, 100, dropout=0.33, bi=True)
-
     model(words)
